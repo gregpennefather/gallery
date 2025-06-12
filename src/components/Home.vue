585c0cd1768ba96f2911933976b5ee8a8fefa0ec
Gee <gregpennefatherdev@gmail.com>
2025-06-12 14:01:38 +0200

    Setting up page and modifying from vue default site

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <p>This is a work in progress...</p>
+</template>
+
+<style scoped>
+p {
+  color: whitesmoke;
+}
+</style>
